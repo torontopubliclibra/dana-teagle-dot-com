9de3ddf508c012385026fbcd9b30f9deed3b24af
torontopubliclibra <dana.r.teagle@gmail.com>
2023-09-18 23:18:09 -0400

    chore: cleaned up scripts and styles, updated README and projects, and replaced injected email with forwarded address

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # dana teagle dot com
 
 ## About
-[dana teagle dot com](https://danateagle.com/) is my portfolio website. I built it using HTML, SCSS, and jQuery. The data for each of the portfolio projects is stored as a JSON object for ease of iterating, filtering, and updating.
+[dana teagle dot com](https://danateagle.com/) is my portfolio website. I built with jQuery, HTML, and SCSS. The data for each of the portfolio projects is stored as a JSON object for ease of iterating, filtering, and updating.
 
 ## Development
-The site was originally built in Oct 2022, and was most recently redesigned and refactored in Aug and Sept 2023.
+The site was originally developed in Oct 2022, and was most recently redesigned and refactored in Aug and Sept 2023.
 
 ## Attributions
 - [jQuery v3.7.0](https://jquery.com/)
--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,13 @@ let app = {
         body: $("body"),
         about: $("#about"),
         nav: $("nav"),
-        hamburger: $(".hamburger-btn"),
-        scrollDown: $(".scroll-down"),
-        scrollTop: $(".scroll-to-top"),
+        hamburgerButton: $(".hamburger-btn"),
+        scrollDownButton: $(".scroll-down"),
+        scrollTopButton: $(".scroll-to-top"),
         projectsContainer: $(".projects-container"),
         projectsNav: $(".projects-nav"),
         projectDescription: $(".project-description"),
-        emailLink: $(".email")
+        emailLink: $(".email"),
     },
 
     // projects data and selected filter
@@ -22,19 +22,14 @@ let app = {
     },
 
     // email address to inject
-    email: "",
+    email: "info@danateagle.com",
 
     // app functions
     functions: {
 
         // inject email link function
         emailInject: () => {
-
-            // save email address
-            app.email = "dana.r.teagle@gmail.com";
-
             app.elements.emailLink.html(`<a href="mailto:${app.email}" target="_blank" title="Send an email to Dana">an email</a>`);
-
         },
 
         // toggle classes to hide or show the nav
@@ -50,13 +45,15 @@ let app = {
             let location = "";
 
             // if the direction is "top", set the location to the top of the page
-            if (direction === "top") {location = 0}
-
+            if (direction === "top") {
+                location = 0;
             // if the direction is anything else, set the location to the top of that section
-            else {location = app.elements[direction].offset().top}
+            } else {
+                location = app.elements[direction].offset().top;
+            };
 
             // smoothly scroll to the location
-            window.scrollTo({top: location, behavior: "smooth"})
+            window.scrollTo({top: location, behavior: "smooth"});
 
             // blur the button after scroll
             document.activeElement.blur();
@@ -70,40 +67,48 @@ let app = {
             let enter = 13;
         
             // if a key is pressed but it is not the enter key, behave normally
-            if (key && (key !== enter)) {return}
-        
+            if (key && (key !== enter)) {
+                return;
             // if a key is pressed and it is the enter key, scroll to the direction specified
-            else {app.functions.scroll(direction)}
+            } else {
+                app.functions.scroll(direction);
+            };
         },
         
         // displaying the projects
         projectDisplay: (filter) => {
 
             // initialize the project array
-            let projectArray = []
+            let projectArray = [];
 
             // save the parameter as the filter
-            app.projects.filter = filter
+            app.projects.filter = filter;
 
             // intialize the filters array with just 'all'
-            let projectFilters = ["All"]
+            let projectFilters = ["All"];
 
             // for each project
             app.projects.data.forEach((project) => {
 
                 // loop through the tags, and then for each one, if that tag isn't already in the filter array, add it to the list
-                project.tags.forEach((tag) => {projectFilters = projectFilters.filter(filter => filter !== tag).concat([tag])})
-            })
+                project.tags.forEach((tag) => {
+                    projectFilters = projectFilters.filter(filter => filter !== tag).concat([tag]);
+                });
+            });
 
             // sort the project filters
             projectFilters = projectFilters.sort((a, b) => {
 
                 // put 'all' first
-                if (a === "All") {return -1}
-                else if (b === "All") {return 1}
+                if (a === "All") {
+                    return -1;
+                } else if (b === "All") {
+                    return 1;
                 // and then sort the rest alphabetically
-                else {return a > b}
-            })
+                } else {
+                    return a > b;
+                };
+            });
 
             // if there are more than 3 project filters
             if (projectFilters.length > 3) {
@@ -115,34 +120,57 @@ let app = {
                     let filterName = '';
 
                     // if the parameter is 'all', save that as the filter
-                    if (filter === "All") {filterName = filter}
+                    if (filter === "All") {
+                        filterName = filter;
 
                     // if the filter is anything else, save that as the filter name with a hashtag in front of it
-                    else {filterName = `#` + filter}
+                    } else {
+                        filterName = `#` + filter;
+                    };
 
                     // if the filter is already selected, create a span for it with the selected class
-                    if (filter === app.projects.filter) {return `<li class="selected">` + filterName + `</li>`}
+                    if (filter === app.projects.filter) {
+                        return `<li class="selected">`
+                        + filterName
+                        + `</li>`;
 
                     // otherwise create a link that displays the projects of that filter
-                    else {return `<li><a onclick="app.functions.projectDisplay('${filter}')" title="${filter} projects">` + filterName + `</a></li>`}
-                })
+                    } else {
+                        return `<li><a onclick="app.functions.projectDisplay('${filter}')" title="${filter} projects">`
+                        + filterName
+                        + `</a></li>`;
+                    };
+                });
 
                 // stitch the html for each of the filters together and add it to the projects nav
-                app.elements.projectsNav.html(`<p>Filter by technology:</p><ul class="project-filters">` + projectFilters.reduce((accumulator, tag) => {return accumulator + tag}) + `</ul></h3>`);
-            }
+                app.elements.projectsNav.html(`
+                    <p>Filter by technology:</p><ul class="project-filters">`
+                    + projectFilters.reduce((accumulator, tag) => {
+                        return accumulator + tag
+                    })
+                    + `</ul></h3>`
+                );
+            };
 
             // sort the projects by year
             let sortedByYear = app.projects.data.sort((a, b) => {
-                if (b.year > a.year) {return 1}
-                if (a.year < b.year) {return -1}
-                else {return 0}
-            })
+                if (b.year > a.year) {
+                    return 1;
+                } if (a.year < b.year) {
+                    return -1;
+                } else {
+                    return 0;
+                };
+            });
 
             // if the selected filter is 'all', fill the array with the sorted projects
-            if (app.projects.filter == "All") {projectArray = sortedByYear}
+            if (app.projects.filter == "All") {
+                projectArray = sortedByYear;
 
             // if the selected filter is anything else, fill the array with the sorted projects, filtered by the selected filter
-            else {projectArray = sortedByYear.filter((project) => project.tags.includes(app.projects.filter))}
+            } else {
+                projectArray = sortedByYear.filter((project) => project.tags.includes(app.projects.filter));
+            };
 
             // map out the sorted projects to the page
             let formattedProjects = projectArray.map((project) => {
@@ -153,7 +181,7 @@ let app = {
                     description: "",
                     site: "",
                     code: "",
-                }
+                };
 
                 // format the project heading
                 formattedProject.heading = `<h3>` + project.title + `</h3>`
@@ -162,24 +190,47 @@ let app = {
                 if (project.description.length > 0) {
                     
                     // format the paragraph
-                    let formattedParagraph = project.description.reduce((accumulator, paragraph) => {return accumulator +  `</br></br>` + paragraph})
+                    let formattedParagraph = project.description.reduce((accumulator, paragraph) => {
+                        return accumulator
+                        + `</br></br>`
+                        + paragraph
+                    });
 
                     // and combine that paragraph with a read more button
-                    formattedProject.description = `<button class="button read-more" title="` + project.title + ` project description">Description <i class="fa fa-angle-down"></i></button><p>` + formattedParagraph + `</p>`
+                    formattedProject.description =
+                        `<button class="button read-more" title="`
+                        + project.title +
+                        ` project description">Description <i class="fa fa-angle-down"></i></button><p>`
+                        + formattedParagraph
+                        + `</p>`
                 }
 
                 // if the project has a site link, format a site button
-                if (project.site) {formattedProject.site = `<a href="${project.site}" target="_blank" class="button" title="${project.title} website">Site <i class="fa fa-external-link-square" aria-hidden="true"></i></a>`}
+                if (project.site) {
+                    formattedProject.site =
+                        `<a href="${project.site}" target="_blank" class="button" title="${project.title} website">Site <i class="fa fa-external-link-square" aria-hidden="true"></i></a>`
+                    };
 
                 // if the project has a code link, format a code button
-                if (project.code) {formattedProject.code = `<a href="${project.code}" class="button" target="_blank" title="${project.title} repo on Github">Code <i class="fa fa-external-link-square" aria-hidden="true"></i></a>`}
+                if (project.code) {
+                    formattedProject.code =
+                        `<a href="${project.code}" class="button" target="_blank" title="${project.title} repo on Github">Code <i class="fa fa-external-link-square" aria-hidden="true"></i></a>`
+                    };
 
                 // stitch together all the html for the project
-                return `<div class="project">` + formattedProject.heading + `<div class="project-description">` + formattedProject.site + formattedProject.code + formattedProject.description + `</div></div>`
-            })
+                return `<div class="project">`
+                + formattedProject.heading
+                + `<div class="project-description">`
+                + formattedProject.site
+                + formattedProject.code
+                + formattedProject.description
+                + `</div></div>`
+            });
 
             // stitch the html for each of the projects together and add that the projects container
-            app.elements.projectsContainer.html(formattedProjects.reduce((accumulator, project) => {return accumulator + project}))
+            app.elements.projectsContainer.html(formattedProjects.reduce((accumulator, project) => {
+                return accumulator + project;
+            }));
 
             // for each project details section
             document.querySelectorAll(".project-description").forEach((project) => {
@@ -188,13 +239,13 @@ let app = {
                 if (project.querySelector(".read-more")) {
 
                     // store the button and paragraph in variables
-                    let button = project.querySelector(".read-more")
-                    let paragraph = project.querySelector("p")
+                    let button = project.querySelector(".read-more");
+                    let paragraph = project.querySelector("p");
     
                     // run the read more function on the click of the button
-                    button.addEventListener("click", () => app.functions.readMore(project, paragraph, button))
-                }
-            })
+                    button.addEventListener("click", () => app.functions.readMore(project, paragraph, button));
+                };
+            });
         },
 
         // read more function
@@ -228,16 +279,16 @@ let app = {
     events: () => {
 
         // on the hamburger button click, run the hamburger function
-        app.elements.hamburger.click(app.functions.toggleNav);
+        app.elements.hamburgerButton.click(app.functions.toggleNav);
 
         // for each of the nav list item links, toggle the nav on click
         document.querySelectorAll("nav ul li a").forEach((link) => link.addEventListener("click", app.functions.toggleNav));
 
         // scroll up to top of browser window on button click
-        app.elements.scrollTop.click(() => app.functions.scroll("top"))
+        app.elements.scrollTopButton.click(() => app.functions.scroll("top"));
 
         // scroll down to top of about section on button click
-        app.elements.scrollDown.click(() => app.functions.scroll("about"))
+        app.elements.scrollDownButton.click(() => app.functions.scroll("about"));
 
         // inject email link
         app.functions.emailInject();
@@ -249,7 +300,6 @@ let app = {
 
         // fetch the projects from the json file and send the response
         fetch('./data/projects.json').then(response => response.json())
-
             // then with the data
             .then((data) => {
 
@@ -270,26 +320,29 @@ let app = {
                     }
 
                     // then map each property in the initial object to the new object
-                    for (let property in data[object]) {project[property] = data[object][property]}
+                    for (let property in data[object]) {
+                        project[property] = data[object][property];
+                    };
 
                     // and push each project object into the projects array
-                    projects.push(project)
-                }
+                    projects.push(project);
+                };
 
                 // save the projects array to the project data
-                app.projects.data = projects
+                app.projects.data = projects;
 
                 // display the projects on the page using the default filter
-                app.functions.projectDisplay(app.projects.filter)
+                app.functions.projectDisplay(app.projects.filter);
             })
-
             // console log any promise errors
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
 
         // add the event listeners
-        app.events()
-    }
-}
+        app.events();
+    },
+};
 
 // initialize the app
-$(document).ready(function () {app.init()})
+$(document).ready(() => {
+    app.init();
+});
