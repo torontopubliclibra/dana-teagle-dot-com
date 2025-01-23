415eddfe31793c9c704458de8faff9b3b8bac4cd
torontopubliclibra <dana.r.teagle@gmail.com>
2025-01-22 20:49:25 -0500

    feat: updated rss feed and rss formatter to limit the feed to 10 posts

--- rustprop/rss.js
+++ rustprop/rss.js
@@ -14,31 +14,33 @@ let rssFormatter = {
         let items = document.querySelectorAll(`div.item`);
         let formattedItems = ``;
 
-        items.forEach((item) => {
-            let id = `#` + item.id;
-            let date = item.querySelector(`.datestamp`).innerText;
-            let formattedDate = rssFormatter.formatDate(date);
-            let headline = item.querySelector('.headline').innerHTML;
-            let url = item.querySelector('img').src;
-            let image = url.substring(url.search(/images/));
-            let title = item.querySelector('img').alt;
+        items.forEach((item, index) => {
+            if (index < 10) {
+                let id = `#` + item.id;
+                let date = item.querySelector(`.datestamp`).innerText;
+                let formattedDate = rssFormatter.formatDate(date);
+                let headline = item.querySelector('.headline').innerHTML;
+                let url = item.querySelector('img').src;
+                let image = url.substring(url.search(/images/));
+                let title = item.querySelector('img').alt;
 
-            if (title.includes(`&`)) {
-                title = title.replace(`&`, `&amp;`);
+                if (title.includes(`&`)) {
+                    title = title.replace(`&`, `&amp;`);
+                }
+
+                console.log(formattedDate);
+
+                let formattedItem = `
+                <item>
+                    <title>${title}</title>
+                    <link>https://www.danateagle.com/rustprop${id}</link>
+                    <guid>https://www.danateagle.com/rustprop${id}</guid>
+                    <pubDate>${formattedDate}</pubDate>
+                    <description><![CDATA[<img src="https://www.danateagle.com/rustprop/images/${image}" alt="${title}"/><br/>${headline}]]></description>
+                </item>`;
+
+                formattedItems += formattedItem;
             }
-
-            console.log(formattedDate);
-
-            let formattedItem = `
-            <item>
-                <title>${title}</title>
-                <link>https://www.danateagle.com/rustprop${id}</link>
-                <guid>https://www.danateagle.com/rustprop${id}</guid>
-                <pubDate>${formattedDate}</pubDate>
-                <description><![CDATA[<img src="https://www.danateagle.com/rustprop/images/${image}" alt="${title}"/><br/>${headline}]]></description>
-            </item>`;
-
-            formattedItems += formattedItem;
         });
 
         console.log(formattedItems);
