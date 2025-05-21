7db2a9b12fb194cbd1d857abaefbe528cd895bbf
torontopubliclibra <dana.r.teagle@gmail.com>
2025-05-21 18:16:44 -0400

    feat: added toggle for ascii headshot on smaller screens

--- scripts/app.js
+++ scripts/app.js
@@ -686,6 +686,12 @@ let app = {
 
         app.elements.headshotWrapper.click((e) => {
             app.elements.headshotWrapper.toggleClass("clicked");
+            $("#ascii-toggle").toggleClass("highlight");
+            if (app.elements.headshotWrapper.hasClass("clicked")) {
+                $("#ascii-toggle").html(`]<img src="./assets/icons/checkbox.svg" alt="Checked checkbox">[ IMG to ASCII`);
+            } else {
+                $("#ascii-toggle").html(`]<img src="./assets/icons/checkbox-blank.svg" alt="Unchecked checkbox">[ IMG to ASCII`);
+            }
         });
 
         // smooth scroll to top of sections on nav link click
