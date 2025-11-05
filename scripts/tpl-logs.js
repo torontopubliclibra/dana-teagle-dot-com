ca1422b32af4a274966471ad0e9556395427539c
torontopubliclibra <dana.r.teagle@gmail.com>
2025-11-05 09:16:12 -0500

    feat: replace logs and now with data

---
+++ scripts/tpl-logs.js
@@ -0,0 +1,74 @@
+let tplLogs = {
+    content: $(".tpl-page-text"),
+    year: "2025",
+    updated: "05/11/2025",
+    date: $(".date"),
+    movies: [],
+    books: [],
+    // yearSelect: `<p><span class="year-selected">2025</span> | <button class="range" onclick="tplLogs.functions.yearSet('2024')">2024</button></p>`,
+    functions: {
+        // yearSet: (year) => {
+        //     switch(year) {
+        //         case "2":
+        //             tplLogs.year = "2025";
+        //             tplLogs.yearSelect = `<p><span class="year-selected">2025</span> | <button class="range" onclick="tplLogs.functions.yearSet('2024')">2024</button></p>`
+        //             break;
+        //         case "1":
+        //             tplLogs.year = "2024";
+        //             tplLogs.yearSelect = `<p><button class="range" onclick="tplLogs.functions.yearSet('2025')">2025</button> | <span class="year-selected">2024</span></p>`
+        //             break;
+        //     }
+        //     tplLogs.year = year;
+        //     tplLogs.functions.logsDisplay();
+        //     window.scroll(top);
+        // },
+        logsDisplay: () => {
+            let year = tplLogs.year;
+            // let yearSelect = tplLogs.yearSelect;
+            // let formattedLogs = [yearSelect, `<hr><p id="movies">>> movies watched in 2025 (<a href="/letterboxd" target="_blank">letterboxd</a>):</p>`];
+            let formattedLogs = [`<p id="movies">>> watched in 2025 (<a href="/letterboxd" target="_blank">letterboxd</a>):</p>`];
+
+            switch(year) {
+                case "2025":
+                    for (let list in tplLogs.movies) {
+                        let array = [ ...tplLogs.movies[list]];
+                        array.forEach(movie => {
+                            let item = `<p class="sub">> ${movie}</p>`;
+                            formattedLogs.push(item);
+                        });
+                    }
+                    formattedLogs.push(`<hr><p id="books">>> read in 2025 (<a href="/goodreads" target="_blank">goodreads</a>):</p>`);
+                    for (let list in tplLogs.books) {
+                        let array = [ ...tplLogs.books[list]];
+                        array.forEach(book => {
+                            let item = `<p class="sub">> ${book}</p>`;
+                            formattedLogs.push(item);
+                        });
+                    }
+                    break;
+            }
+            tplLogs.content.html(formattedLogs.reduce((accumulator, log) => {
+                return accumulator + log;
+            }));
+            tplLogs.date.text(tplLogs.updated);
+        },
+    },
+    init: () => {
+        fetch('../data/logs.json').then(response => response.json())
+        .then((data) => {
+            let movies = [];
+            let books = [];
+            for (let object in data) {
+                movies.push(data[object]["movies"]);
+                books.push(data[object]["books"]);
+            }
+            tplLogs.movies = movies;
+            tplLogs.books = books;
+            tplLogs.functions.logsDisplay();
+        })
+        .catch(error => console.log(error));
+    },
+};
+$(document).ready(() => {
+    tplLogs.init();
+});
---
+++ scripts/tpl-now.js
@@ -0,0 +1,97 @@
+let tplNow = {
+    content: $(".tpl-page-text"),
+    updated: "",
+    date: $(".date"),
+    list: [],
+    movies: [],
+    books: [],
+    mixes: {},
+    functions: {
+        nowDisplay: () => {
+            let formattedNow = [];
+            for (let list in tplNow.list) {
+                let array = [tplNow.list[list]];
+                array.forEach(bullet => {
+                    let item = `<p>>> ${bullet}</p>`;
+                    formattedNow.push(item);
+                });
+            }
+
+            formattedNow.push(`<hr><p>>> last watched ( <a href="/letterboxd" target="_blank">letterboxd</a> | <a href="/tpl/logs#movies" target="_blank">see more</a> ):</p>`);
+            for (let list in tplNow.movies) {
+                let array = [tplNow.movies[list]];
+                array.forEach(movie => {
+                    let item = `<p class="sub">> ${movie}</p>`;
+                    formattedNow.push(item);
+                });
+            }
+
+            formattedNow.push(`<p>>> last read ( <a href="/goodreads" target="_blank">goodreads</a> | <a href="/tpl/logs#books" target="_blank">see more</a> ):</p>`);
+            for (let list in tplNow.books) {
+                let array = [tplNow.books[list]];
+                array.forEach(book => {
+                    let item = `<p class="sub">> ${book}</p>`;
+                    formattedNow.push(item);
+                });
+            }
+
+            formattedNow.push(`<p>>> latest mixes ( <a href="/tidal" target="_blank">tidal</a> | <a href="/tpl/mixes" target="_blank">see more</a> ):</p>`);
+            for (let mix in tplNow.mixes) {
+                let object = tplNow.mixes[mix];
+                let link = object["tidal"];
+                formattedNow.push(`<p class="sub">> rusty mix #${object.number}: <a href="${link}" target="_blank">${object.title}</a></p>`);
+            }
+
+            tplNow.content.html(formattedNow.reduce((accumulator, log) => {
+                return accumulator + log;
+            }));
+            tplNow.date.text(tplNow.updated);
+        },
+    },
+    init: () => {
+        fetch('../data/now.json').then(response => response.json())
+        .then((data) => {
+            let list = [];
+            for (let object in data) {
+                list.push(data[object]);
+            }
+            tplNow.updated = Object.keys(data)[0];
+            tplNow.list = list[0];
+            tplNow.functions.nowDisplay();
+        })
+        .catch(error => console.log(error));
+
+        fetch('../data/logs.json').then(response => response.json())
+        .then((data) => {
+            let movies = [];
+            let books = [];
+            for (let object in data) {
+                movies.push(data[object]["movies"]);
+                books.push(data[object]["books"]);
+            }
+            tplNow.movies = movies[0].slice(0,3);
+            tplNow.books = books[0].slice(0,3);
+            tplNow.functions.nowDisplay();
+        })
+        .catch(error => console.log(error));
+
+        fetch('../data/mixes.json').then(response => response.json())
+        .then((data) => {
+            let mixes = [];
+            for (let object in data) {
+                let mix = {
+                    "title": object,
+                    "number": data[object]["number"],
+                    "tidal": data[object]["tidal"],
+                };
+                mixes.push(mix);
+            }
+            tplNow.mixes = mixes.slice(0,3);
+            tplNow.functions.nowDisplay();
+        })
+        .catch(error => console.log(error));
+    },
+};
+$(document).ready(() => {
+    tplNow.init();
+});
