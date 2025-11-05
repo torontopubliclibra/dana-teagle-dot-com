c60ea3864d939952cafe410113417082bd13e1a3
torontopubliclibra <dana.r.teagle@gmail.com>
2025-11-04 23:35:48 -0500

    feat: added more mixes

--- scripts/tpl-mixes.js
+++ scripts/tpl-mixes.js
@@ -1,28 +1,20 @@
 let tplMixes = {
     content: $(".tpl-page-text"),
     mixes: {},
-    range: "4",
+    range: "2",
     stream: "tidal",
     streamSelect: `<p>streaming links: <span class="range-selected">tidal</span> | <button class="range" onclick="tplMixes.functions.streamSet('spotify')">spotify</button></p>`,
-    rangeSelect: `<p>>> <span class="range-selected">#196-151</span> | <button class="range" onclick="tplMixes.functions.rangeSet('3')">#150-101</button> | <button class="range" onclick="tplMixes.functions.rangeSet('2')">#100-51</button> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#50-1</button></p>`,
+    rangeSelect: `<p>>> <span class="range-selected">#196-101</span> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#100-1</button></p>`,
     functions: {
         rangeSet: (range) => {
             switch(range) {
-                case "4":
-                    tplMixes.range = "4";
-                    tplMixes.rangeSelect = `<p>>> <span class="range-selected">#196-151</span> | <button class="range" onclick="tplMixes.functions.rangeSet('3')">#150-101</button> | <button class="range" onclick="tplMixes.functions.rangeSet('2')">#100-51</button> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#50-1</button></p>`
-                    break;
-                case "3":
-                    tplMixes.range = "3";
-                    tplMixes.rangeSelect = `<p>>> <button class="range" onclick="tplMixes.functions.rangeSet('4')">#196-151</button> | <span class="range-selected">#150-101</span> | <button class="range" onclick="tplMixes.functions.rangeSet('2')">#100-51</button> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#50-1</button></p>`
-                    break;
                 case "2":
                     tplMixes.range = "2";
-                    tplMixes.rangeSelect = `<p>>> <button class="range" onclick="tplMixes.functions.rangeSet('4')">#196-151</button> | <button class="range" onclick="tplMixes.functions.rangeSet('3')">#150-101</button> | <span class="range-selected">#100-51</span> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#50-1</button> </p>`
+                    tplMixes.rangeSelect = `<p>>> <span class="range-selected">#196-101</span> | <button class="range" onclick="tplMixes.functions.rangeSet('1')">#100-1</button></p>`
                     break;
                 case "1":
                     tplMixes.range = "1";
-                    tplMixes.rangeSelect = `<p>>> <button class="range" onclick="tplMixes.functions.rangeSet('4')">#196-151</button> | <button class="range" onclick="tplMixes.functions.rangeSet('3')">#150-101</button> | <button class="range" onclick="tplMixes.functions.rangeSet('2')">#100-51</button> | <span class="range-selected">#50-1</span> </p>`
+                    tplMixes.rangeSelect = `<p>>> <button class="range" onclick="tplMixes.functions.rangeSet('2')">#196-101</button> | <span class="range-selected">#100-1</span></p>`
                     break;
             }
             tplMixes.range = range;
@@ -56,51 +48,17 @@ let tplMixes = {
             }
 
             switch(range) {
-                case "4":
-                    for (let mix in tplMixes.mixes) {
-                        let object = tplMixes.mixes[mix];
-                        let count = Number(object.number);
-                        let link = object[stream];
-
-                        if (count >= 151 && count <= 200 && link) {
-                            let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon" alt="external link icon">`;
-                            let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
-                            formattedMixes.push(`<a href="${link}" target="_blank" class="sub mix">${image}${title}</a>`)
-                        } else if (count >= 151 && count <= 200 && !link) {
-                            let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon disabled" alt="external link icon">`;
-                            let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
-                            formattedMixes.push(`<div class="sub mix disabled">${image}${title}</div>`)
-                        }
-                    }
-                    break;
-                case "3":
-                    for (let mix in tplMixes.mixes) {
-                        let object = tplMixes.mixes[mix];
-                        let count = Number(object.number);
-                        let link = object[stream];
-
-                        if (count >= 101 && count <= 150 && link ) {
-                            let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon" alt="external link icon">`;
-                            let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
-                            formattedMixes.push(`<a href="${link}" target="_blank" class="sub mix">${image}${title}</a>`)
-                        } else if (count >= 101 && count <= 150 && !link) {
-                            let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon disabled" alt="external link icon">`;
-                            let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
-                            formattedMixes.push(`<div class="sub mix disabled">${image}${title}</div>`)
-                        }
-                    }
-                    break;
                 case "2":
                     for (let mix in tplMixes.mixes) {
                         let object = tplMixes.mixes[mix];
                         let count = Number(object.number);
                         let link = object[stream];
 
-                        if (count >= 51 && count <= 100 && link) {
+                        if (count >= 101 && count <= 200 && link) {
                             let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon" alt="external link icon">`;
                             let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
                             formattedMixes.push(`<a href="${link}" target="_blank" class="sub mix">${image}${title}</a>`)
-                        } else if (count >= 51 && count <= 100 && !link) {
+                        } else if (count >= 101 && count <= 200 && !link) {
                             let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon disabled" alt="external link icon">`;
                             let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
                             formattedMixes.push(`<div class="sub mix disabled">${image}${title}</div>`)
@@ -113,12 +71,11 @@ let tplMixes = {
                         let count = Number(object.number);
                         let link = object[stream];
 
-                        if (count >= 1 && count <= 50 && link) {
+                        if (count <= 100 && link ) {
                             let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon" alt="external link icon">`;
-                            let href = `href="${object.href}"`;
                             let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
-                            formattedMixes.push(`<a href="${href}" target="_blank" class="sub mix">${image}${title}</a>`)
-                        } else if (count >= 1 && count <= 50 && !link) {
+                            formattedMixes.push(`<a href="${link}" target="_blank" class="sub mix">${image}${title}</a>`)
+                        } else if (count <= 100 && !link) {
                             let title = `<p><small>#${object.number} \\\\</small><br/>${object.title}</p><img src="../assets/icons/external-link.svg" class="icon disabled" alt="external link icon">`;
                             let image = `<img src="${object.image}" alt="${object.title} cover art"/>`;
                             formattedMixes.push(`<div class="sub mix disabled">${image}${title}</div>`)
