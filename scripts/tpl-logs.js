d1c50f02a558dcfc02f84fdd179dfcf0ae3ede3f
torontopubliclibra <dana.r.teagle@gmail.com>
2025-12-06 07:13:10 -0500

    chore: updated now, logs, feed

--- scripts/tpl-logs.js
+++ scripts/tpl-logs.js
@@ -1,7 +1,7 @@
 let tplLogs = {
     content: $(".tpl-page-text"),
     year: "2025",
-    updated: "23/11/2025",
+    updated: "06/12/2025",
     date: $(".date"),
     twentyFive: {
         movies: [],
