27000ceca8da3ac1aaefcd6b6816b6830ad8a4db
torontopubliclibra <dana.r.teagle@gmail.com>
2026-07-06 15:25:17 -0400

    chore: updated CORS headers for json

--- netlify/edge-functions/restricted-cors-json.ts
+++ netlify/edge-functions/restricted-cors-json.ts
@@ -1,14 +1,14 @@
-const LOCALHOST_ORIGIN = /^http:\/\/localhost(?::\d+)?$/;
+const LOCALHOST_ORIGIN = /^http:\/\/(localhost|127\.0\.0\.1)(?::\d+)?$/;
 const ALLOWED_ORIGIN = "https://clouds.danateagle.com";
 
 export default async (request: Request, context: { next: () => Promise<Response> }) => {
-  const response = await context.next();
-  const origin = request.headers.get("origin");
+    const response = await context.next();
+    const origin = request.headers.get("origin");
 
-  if (origin && (origin === ALLOWED_ORIGIN || LOCALHOST_ORIGIN.test(origin))) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
-    response.headers.set("Vary", "Origin");
-  }
+    if (origin && (origin === ALLOWED_ORIGIN || LOCALHOST_ORIGIN.test(origin))) {
+        response.headers.set("Access-Control-Allow-Origin", origin);
+        response.headers.set("Vary", "Origin");
+    }
 
-  return response;
+    return response;
 };
